7dca75d97af28224a607622ab0bc8f82ffcb7381
brian-rg <brianreyesg@hotmail.com>
2022-04-05 23:28:25 -0500

    agregada una imagen
    Signed-off-by: brian-rg <brianreyesg@hotmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -12,8 +13,9 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to Brian application" />
+        <Image src="./peru.png"></Image>
         <p className="description">
-          Puedes ver el repositorio de esta página <a href="https://github.com/Brian-RG/next-netlify-starter">aquí</a>
+          Puedes ver el repositorio de esta página <a href="https://github.com/Brian-RG/next-netlify-starter" target="_blank">aquí</a>
         </p>
       </main>
 
