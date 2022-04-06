1d93dd0ff10e12b062ab67bcdd5d8ee20cf23808
brian-rg <brianreyesg@hotmail.com>
2022-04-05 22:51:32 -0500

    changing index content
    Signed-off-by: brian-rg <brianreyesg@hotmail.com>

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>My application</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Brian application" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Puedes ver el repositorio de esta página <a href="https://github.com/Brian-RG/next-netlify-starter">aquí</a>
         </p>
       </main>
 
